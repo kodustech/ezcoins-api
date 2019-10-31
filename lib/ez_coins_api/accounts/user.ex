16db3c0e48a93cd20543fdc9c0a26beeb26e6170
maykon@deepin <maykonmichelpalma@gmail.com>
2019-10-31 14:14:19 -0300

    Added hash password function

--- lib/ez_coins_api/accounts/user.ex
+++ lib/ez_coins_api/accounts/user.ex
@@ -5,6 +5,8 @@ defmodule EzCoinsApi.Accounts.User do
   schema "users" do
     field :email, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
@@ -12,7 +14,24 @@ defmodule EzCoinsApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :password_hash])
-    |> validate_required([:email, :password_hash])
+    |> cast(attrs, [:email, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 6, max: 50)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email)
+    |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{
+        valid?: true,
+        changes: %{
+          password: password
+        }
+      } -> change(changeset, Argon2.add_hash(password))
+      _ -> changeset
+    end
   end
 end
