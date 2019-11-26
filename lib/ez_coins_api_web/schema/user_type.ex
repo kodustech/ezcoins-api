ce22756dd6627a08e1eeaabc3a2670d2712bf851
maykon@deepin <maykonmichelpalma@gmail.com>
2019-11-26 10:33:36 -0200

    Added name and avatar fields to user type schema

--- lib/ez_coins_api_web/schema/user_type.ex
+++ lib/ez_coins_api_web/schema/user_type.ex
@@ -7,13 +7,17 @@ defmodule EzCoinsApiWeb.Schema.Types.UserType do
 
   object :user_type do
     field(:id, :id)
+    field(:name, :string)
     field(:email, :string)
+    field(:avatar, :string)
     field(:wallet, :wallet_type, resolve: &UserResolver.wallet/3)
   end
 
   input_object :user_input_type do
+    field(:name, :string)
     field(:email, non_null(:string))
     field(:password, non_null(:string))
     field(:password_confirmation, non_null(:string))
+    field(:avatar, :string)
   end
 end
