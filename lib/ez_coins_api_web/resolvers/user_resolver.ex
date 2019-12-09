2a07d795b44f840dac537a8927ac5b4dbe8621d2
maykonmichel <maykonmichelpalma@gmail.com>
2019-12-09 10:23:27 -0200

    Added list active users only

--- lib/ez_coins_api_web/resolvers/user_resolver.ex
+++ lib/ez_coins_api_web/resolvers/user_resolver.ex
@@ -11,7 +11,7 @@ defmodule EzCoinsApiWeb.Resolvers.UserResolver do
   def users(_, args, %{context: context}) do
     case args do
       %{except_me: true} -> {:ok, Accounts.list_users_except_id(context.current_user.id)}
-      _ -> {:ok, Accounts.list_users()}
+      _ -> {:ok, Accounts.list_active_users()}
     end
   end
 
