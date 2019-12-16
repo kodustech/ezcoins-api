e75c20bf7c02dfc694ce8f31626a0949761a139b
maykonmichel <maykonmichelpalma@gmail.com>
2019-12-16 16:03:33 -0200

    Removed donate at of donations table

--- lib/ez_coins_api/finances/donation.ex
+++ lib/ez_coins_api/finances/donation.ex
@@ -3,7 +3,6 @@ defmodule EzCoinsApi.Finances.Donation do
   import Ecto.Changeset
 
   schema "donations" do
-    field :donate_at, :date
     field :quantity, :integer
     field :reason, :string
     field :receiver_user_id, :id
@@ -15,8 +14,8 @@ defmodule EzCoinsApi.Finances.Donation do
   @doc false
   def changeset(donation, attrs) do
     donation
-    |> cast(attrs, [:donate_at, :quantity, :reason, :receiver_user_id, :sender_user_id])
-    |> validate_required([:donate_at, :quantity, :reason, :receiver_user_id, :sender_user_id])
+    |> cast(attrs, [:quantity, :reason, :receiver_user_id, :sender_user_id])
+    |> validate_required([:quantity, :reason, :receiver_user_id, :sender_user_id])
     |> validate_number(:quantity, greater_than: 0)
     |> validate_length(:reason, min: 5)
   end
