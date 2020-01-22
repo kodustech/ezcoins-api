4e96423c6705e450c3200e9bf30ba3144db73700
maykonmichel <maykonmichelpalma@gmail.com>
2020-01-22 15:09:01 -0200

    Added DB_NAME environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # shellcheck disable=SC2046
+
 if [ -f ".env" ]; then
-  export "$(cat .env)"
+  export $(cat .env)
 fi
 
-while ! pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER
-do
+while ! pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER; do
   echo "$(date) - $DATABASE_URL - waiting for database to start"
   sleep 2
 done
