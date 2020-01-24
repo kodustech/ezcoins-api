cd92d1e3396b11b64ab6cf086ef84f37a756d67d
maykonmichel <maykonmichelpalma@gmail.com>
2020-01-24 18:33:28 -0200

    Added users seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,3 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     EzCoinsApi.Repo.insert!(%EzCoinsApi.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+import Code, only: [require_file: 2]
+
+require_file("users.exs", "priv/repo/seeds")
