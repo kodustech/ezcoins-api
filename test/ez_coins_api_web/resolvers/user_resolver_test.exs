ba443936091dae10d80c1c54a11809f33776137f
maykonmichel <maykonmichelpalma@gmail.com>
2020-02-20 11:39:48 -0300

    Added create_user with valid data creates a user test

--- test/ez_coins_api_web/resolvers/user_resolver_test.exs
+++ test/ez_coins_api_web/resolvers/user_resolver_test.exs
@@ -2,6 +2,15 @@ defmodule EzCoinsApi.UserResolverTest do
   use EzCoinsApiWeb.ConnCase
   use EzCoinsApi.Fixtures, [:user]
 
+  @valid_attrs %{
+    avatar: "some avatar uri",
+    name: "some name",
+    email: "some@email.com",
+    password: "same password",
+    password_confirmation: "same password",
+    hired_at: "2017-10-29"
+  }
+
   def user_to_map(user) do
     %{
       "id" => "#{user.id}",
@@ -74,6 +83,43 @@ defmodule EzCoinsApi.UserResolverTest do
         |> json_response(200)
 
       assert data["user"] == user
+    end
+  end
+
+  describe "user mutation" do
+    test "create_user with valid data creates a user", %{conn: conn} do
+      query = """
+        mutation($input: UserInputType!) {
+          create_user(input: $input) {
+            id
+            avatar
+            name
+            email
+            hired_at
+            resigned_at
+          }
+        }
+      """
+
+      variables = %{
+        input: @valid_attrs
+      }
+
+      %{
+        "data" => %{
+          "create_user" => user
+        }
+      } =
+        conn
+        |> post("/graphql", %{query: query, variables: variables})
+        |> json_response(200)
+
+      refute user["id"] == nil
+      assert user["avatar"] == @valid_attrs.avatar
+      assert user["name"] == @valid_attrs.name
+      assert user["email"] == @valid_attrs.email
+      assert user["hired_at"] == @valid_attrs.hired_at
+      assert user["resigned_at"] == nil
     end
   end
 end
