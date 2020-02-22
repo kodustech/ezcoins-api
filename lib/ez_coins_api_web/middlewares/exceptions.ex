b427812db4927cb7b849c54fb8b6885e11e8ca3d
maykonmichel <maykonmichelpalma@gmail.com>
2020-02-22 12:21:07 -0300

    Added resign_user with invalid data and admin authenticated returns fields errors test

--- lib/ez_coins_api_web/middlewares/exceptions.ex
+++ lib/ez_coins_api_web/middlewares/exceptions.ex
@@ -8,22 +8,18 @@ defmodule EzCoinsApiWeb.Middlewares.Exceptions do
   end
 
   defp to_fun({{module, function}, config}, res, _config) do
-    IO.inspect('f1')
     fn -> apply(module, function, [res, config]) end
   end
 
   defp to_fun({module, config}, res, _config) do
-    IO.inspect('f2')
     fn -> apply(module, :call, [res, config]) end
   end
 
   defp to_fun(module, res, config) when is_atom(module) do
-    IO.inspect('f3')
     fn -> apply(module, :call, [res, config]) end
   end
 
   defp to_fun(fun, res, config) when is_function(fun, 2) do
-    IO.inspect('f4')
     fn -> fun.(res, config) end
   end
 
